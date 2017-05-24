9e84db36d2d287001a1e33095158603763f57ba8
diarmaidm <diarmaidm@users.noreply.github.com>
2017-05-25 00:56:22 +0100

    make home#index the default root. Create new page and controller for about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @message = "Hello, how are you today?"
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+
+  root 'home#index'
+
+  get '/about' => 'home#about'
+  # get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
